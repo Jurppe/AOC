4670bf00eb78b12a8cfb6f9c7382413776097f0f
Mika Järvenpää <jarskela@gmail.com>
2019-02-16 16:59:56 +0200

    Day 2 part 2 of the Challenge

--- Day2p2.py
+++ Day2p2.py
@@ -2,37 +2,35 @@
 
 def read_input():
     input_data = open('input2.txt', 'r')
-    words_data = {}
+    words_data = []
     for word in input_data:
-        processed_word = analyze_word(word)
-        words_data[word] = processed_word
-
-
-def analyze_word(word):
-    letter_set = {}
-    for letter in word:
-        if letter not in letter_set:
-            letter_set[letter] = 1
-        else:
-            letter_set[letter] += 1
-
-    return letter_set
+        for comparable in words_data:
+            if compare_words(word, comparable):
+                print("Stopping process")
+                return 0
+        words_data.append(word)
 
 
 def compare_words(first, second):
     misses = 0
+    i = 0
 
-    for i in first:
+    print(f"comparing words {first} and {second}")
+
+    while i < len(first):
         if first[i] != second[i]:
             misses += 1
+        i += 1
 
     if misses == 0:
         print("Sanat oli samat?")
 
     elif misses == 1:
-        return first, second
+        print(f"######## Correct words were {first} and {second} ########")
+        return True
 
     else:
         return False
+
 
 read_input()
