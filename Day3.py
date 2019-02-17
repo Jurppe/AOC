0eb586ae3c91f003e34a853560173d06258f74d8
Mika Järvenpää <jarskela@gmail.com>
2019-02-17 12:13:19 +0200

    Day 2 part 2 of the Challenge

--- Day3.py
+++ Day3.py
@@ -1,11 +1,22 @@
 import re
 
+map = {}
+
 
 def read_input():
-    input_file = open("input3", "r")
+    input_file = open("input3_test", "r")
 
     for line in input_file:
         line = trim_line(line)
+        draw_spots_to_map(int(line[0]), int(line[2]), int(line[4]), int(line[6]), int(line[8]))
+
+
+def draw_spots_to_map(id, from_left, from_top, wide, tall):
+
+    for i in range(tall):
+        for j in range(wide):
+            map[from_left + j, from_top + i] = id
+
 
 
 def trim_line(line):
